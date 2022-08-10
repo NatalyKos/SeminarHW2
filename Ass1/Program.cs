fa80404730ada48159c37a4ed707ec3fd47dfb28
Nataly K <doublejanus4@gmail.com>
2022-08-10 20:33:02 +0300

    Ass1

---
+++ Ass1/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Enter a 3-digit number: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+int twoFirst = Number / 10;
+int secondDigit = twoFirst % 10;
+Console.WriteLine(secondDigit);
