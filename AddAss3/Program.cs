c1a1d681fa49f096a3764fed41e95a474e807581
Nataly K <doublejanus4@gmail.com>
2022-08-10 22:15:48 +0300

    AddAss3

---
+++ AddAss3/Program.cs
@@ -0,0 +1,31 @@
+﻿int Number = ReadInt("Введите число: ");
+int Answer = 10;
+int Count = 3;
+while(Count != 0)
+{
+    if(Number == Answer)
+    {
+        Console.WriteLine("Correct!");
+        break;
+    }
+    else
+    {
+        Count--;
+        if(Answer > Number)
+        {
+            Number = ReadInt("Try a bigger number: ");
+
+        }
+        else
+        {
+            Number = ReadInt("Try a lesser number: ");
+        }
+    }
+} 
+
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
