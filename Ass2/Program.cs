1d7953b0c78b95f81a1b7d04795e9def712c2273
Nataly K <doublejanus4@gmail.com>
2022-08-10 21:31:43 +0300

    Ass2

---
+++ Ass2/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Enter a number: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+
+
+
+if(Number < 100)
+{
+    Console.WriteLine("третьей цифры нет");
+}
+else
+{
+   while(Number > 999)
+   {
+        Number = Number / 10;
+   }
+   Number = Number % 10;
+   Console.WriteLine(Number);
+}
+
+
+// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
