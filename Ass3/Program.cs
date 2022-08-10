fa9dd5457ff6927dc1fd7cee39b56e2dd798171b
Nataly K <doublejanus4@gmail.com>
2022-08-10 21:53:06 +0300

    Ass3

---
+++ Ass3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+int Number = ReadInt("Введите число: ");
+
+
+if(Number == 7 || Number == 6)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
+
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
