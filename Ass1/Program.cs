389d5daa9d07fc31d2f5d7b90a2f5b2030bdd781
Nataly K <doublejanus4@gmail.com>
2022-08-10 20:34:44 +0300

    Ass1

--- Ass1/Program.cs
+++ Ass1/Program.cs
@@ -3,3 +3,9 @@ int Number = Convert.ToInt32(Console.ReadLine());
 int twoFirst = Number / 10;
 int secondDigit = twoFirst % 10;
 Console.WriteLine(secondDigit);
+
+// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
